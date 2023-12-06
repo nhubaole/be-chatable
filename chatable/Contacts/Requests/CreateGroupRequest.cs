7d04dff3558c77f301b047ce4bb9e84288508dcd
nhubaole <okamisameko7123@gmail.com>
2023-12-06 15:42:21 +0700

    create group/get all groups

---
+++ chatable/Contacts/Requests/CreateGroupRequest.cs
@@ -0,0 +1,10 @@
+﻿using chatable.Models;
+
+namespace chatable.Contacts.Requests
+{
+    public class CreateGroupRequest
+    {
+        public string GroupName { get; set; }
+        public List<string> MemberList { get; set; }
+    }
+}
