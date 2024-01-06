288f53bda1c1a8447efc98a55bbdb05e366bc9b3
nhubaole <okamisameko7123@gmail.com>
2024-01-06 16:37:23 +0700

    check if user is friend

--- chatable/Controllers/UserController.cs
+++ chatable/Controllers/UserController.cs
@@ -16,6 +16,7 @@ namespace chatable.Controllers
 
         // Get user by id
         [HttpGet("{UserName}")]
+        [Authorize]
         public async Task<ActionResult<User>> GetUserById(string UserName, [FromServices] Client client)
         {
             var currentUser = GetCurrentUser();
@@ -23,6 +24,7 @@ namespace chatable.Controllers
             {
                 var response = await client.From<User>().Where(x => x.UserName == UserName).Get();
                 var user = response.Models.FirstOrDefault();
+                bool isFriend;
 
                 if (user is null)
                 {
@@ -35,6 +37,19 @@ namespace chatable.Controllers
                 //    FullName = user.FullName,
                 //    CreateAt = user.CreatedAt
                 //};
+
+                //check isFriend
+                var friendResponse = await client.From<Friend>().Where(x => x.FriendId == UserName && x.UserId == currentUser.UserName).Get();
+                var friend = friendResponse.Models.FirstOrDefault();
+                if(friend != null)
+                {
+                    isFriend = true;
+                }
+                else
+                {
+                    isFriend = false;
+                }
+
                 var userResponse = new ProfileUser
                 {
                     UserName = user.UserName,
@@ -42,7 +57,8 @@ namespace chatable.Controllers
                     Email = user.Email,
                     DOB = user.DOB,
                     Gender = user.Gender,
-                    AvatarUrl = user.Avatar
+                    AvatarUrl = user.Avatar,
+                    isFriend = isFriend
                 };
 
                 return Ok(new ApiResponse
@@ -64,8 +80,10 @@ namespace chatable.Controllers
 
 
         [HttpGet]
+        [Authorize]
         public async Task<ActionResult<User>> GetAllUsers(Client client)
         {
+            var currentUser = GetCurrentUser();
             try
             {
                 var response = await client.From<User>().Get();
@@ -78,7 +96,20 @@ namespace chatable.Controllers
                 List<ProfileUser> result = new List<ProfileUser>();
                 foreach (var user in users)
                 {
-                    var usersResponse = new ProfileUser
+                    bool isFriend;
+                    //check isFriend
+                    var friendResponse = await client.From<Friend>().Where(x => x.FriendId == user.UserName && x.UserId == currentUser.UserName).Get();
+                    var friend = friendResponse.Models.FirstOrDefault();
+                    if (friend != null)
+                    {
+                        isFriend = true;
+                    }
+                    else
+                    {
+                        isFriend = false;
+                    }
+
+                    var userResponse = new ProfileUser
                     {
                         UserName = user.UserName,
                         FullName = user.FullName,
@@ -86,9 +117,10 @@ namespace chatable.Controllers
                         DOB = user.DOB,
                         Gender = user.Gender,
                         AvatarUrl = user.Avatar,
+                        isFriend = isFriend
                     };
 
-                    result.Add(usersResponse);
+                    result.Add(userResponse);
                 }
 
                 return Ok(new ApiResponse
